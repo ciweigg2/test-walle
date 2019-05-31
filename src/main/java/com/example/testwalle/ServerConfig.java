ae1c18e3387b77bdb387e90379fc3fc4ba83cc88
maxiucheng <maxiucheng@example.com>
2019-05-31 18:43:20 +0800

    添加docker构建呀

---
+++ src/main/java/com/example/testwalle/ServerConfig.java
@@ -0,0 +1,35 @@
+package com.example.testwalle;
+
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @author 马秀成
+ * @date 2019/5/31
+ * @jdk.version 1.8
+ * @desc
+ */
+@Component
+public class ServerConfig  implements ApplicationListener<WebServerInitializedEvent> {
+    private int serverPort;
+
+    public String getUrl() {
+        InetAddress address = null;
+        try {
+            address = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return "http://"+address.getHostAddress() +":"+this.serverPort;
+    }
+
+    @Override
+    public void onApplicationEvent(WebServerInitializedEvent event) {
+        this.serverPort = event.getWebServer().getPort();
+    }
+
+}
